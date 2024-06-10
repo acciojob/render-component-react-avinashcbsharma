2bc0ff01e25fa88c6279961a518522c421c290f9
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-06-10 16:49:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import '../styles/App.css';
-import React, { useEffect } from 'react';
+import React, from 'react';
 
 const App = () => {
-  useEffect(() => {
-    console.log('The p tag has been rendered.');
-  }, []);
+
   return (
-    <div>
+    <div id = 'main'>
       <h1>React Learning</h1>
       <p> I am learning React. My life is getting better. </p>
     </div>
