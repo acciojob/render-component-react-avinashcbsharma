b144ee81e268807a651ef229862457b39fdd409c
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-06-10 16:29:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1>React Learning</h1>
-      <p> <a>I am learning React. My life is getting better.</a></p>
+      <p> I am learning React. My life is getting better. </p>
     </div>
   );
 }
