e8f68a6d0ddf41d0dce09b27e856314ac85af3e3
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-06-10 16:22:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
+    <div id = 'root'>
       <h1>Learning React</h1>
       <p>I am learning React. My life is getting better.</p>
     </div>
